9c55fd82600e21d685f649927351e2400a17c569
Jason <ljis120301@gmail.com>
2025-06-03 19:56:02 -0700

    finsh touch

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["0.0.0.0:3003"],
+    },
+  },
+  // Configure the server port
+  async headers() {
+    return []
+  },
 };
 
 export default nextConfig;
